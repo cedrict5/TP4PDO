e0663a573a3e8e7ee764cfe60138992fcb49fc45
cedrict5 <cedrictiana78@gmail.com>
2025-02-03 11:17:20 +0100

    Création des classes modèles video2 7:20

---
+++ modeles/Continents.php
@@ -0,0 +1,47 @@
+<?php 
+    class Continent{
+        /**
+         * numero du continent
+         * @var int
+         */
+        private $num;
+
+        /**
+         * libelle du continent
+         * @var string
+         */
+        private $libelle;
+
+
+
+        /**
+         * Get the value of num
+         */ 
+        public function getNum()
+        {
+            return $this->num;
+        }
+
+        /**
+         * lit le libellé
+         *
+         * @return string
+         */
+        public function getLibelle() : string
+        {
+            return $this->libelle;
+        }
+
+        /**
+         * ecrit ds le libelle
+         *
+         * @param string $libelle
+         * @return self
+         */
+        public function setLibelle(string $libelle) : self
+        {
+            $this->libelle = $libelle;
+
+            return $this;
+        }
+    }
